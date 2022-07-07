015ed9ce2c94b9ef6c6532a71fbb4f1062097ada
Hooshyar <hooseyr@gmail.com>
2022-07-07 16:55:45 +0300

    vote up post req implemented

--- lib/src/kurdish_names/services/kurdish_names_service.dart
+++ lib/src/kurdish_names/services/kurdish_names_service.dart
@@ -2,12 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:kurdish_names/src/kurdish_names/models/names_data_model.dart';
 
 class KurdishNamesService {
-// https://github.com/DevelopersTree/nawikurdi
-// https://nawikurdi.com/
-
-//API end point : https://nawikurdi.com/api
-
-  Future<KurdishNames> fetchListOfNames() async {
+  Future<KurdishNames> fetchListOfNames(String gender) async {
     //TODO: Create the URI
     // scheme : https , host: nawikurdi.com , path: api ,
 
@@ -18,19 +13,27 @@ class KurdishNamesService {
 //     fragment: 'numbers');
 // print(httpsUri);
 
-    Uri _kurdishNamesUri = Uri(
-        scheme: 'https',
-        host: 'nawikurdi.com',
-        path: 'api',
-        queryParameters: {"limit": "10", "gender": "F", "offset": "0"});
+    // Uri _kurdishNamesUri = Uri(
+    //     scheme: 'https',
+    //     host: 'nawikurdi.com',
+    //     path: 'api',
+    //     queryParameters: {"limit": "10", "gender": gender, "offset": "0"});
 
     http.Response _response = await http
-        .get(Uri.parse('https://nawikurdi.com/api?limit=5&gender=F&offset=0'))
+        .get(Uri.parse(
+            'https://nawikurdi.com/api?limit=20&gender=$gender&offset=0'))
         .catchError((err) => print(err));
 
     KurdishNames _kurdishNames = KurdishNames.fromJson(_response.body);
-
-    print(_response.body);
     return _kurdishNames;
+  }
+
+  //to vote up
+  Future voteUp({required String name_id}) async {
+    await http.post(Uri.parse("https://nawikurdi.com/api/vote"), body: {
+      "name_id": name_id,
+      "uid": "sdafadsdsafsdfasd",
+      "impact": "positive"
+    });
   }
 }
